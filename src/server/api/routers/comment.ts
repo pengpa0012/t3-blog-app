ee4f8d9555ae9b9bfd369cfb4d7ada69b343377f
Pengpa0012 <peng.ilikha@gmail.com>
2023-06-03 15:52:17 +0800

    add create comment

--- src/server/api/routers/comment.ts
+++ src/server/api/routers/comment.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
@@ -6,6 +7,22 @@ import {
 export const postRouter = createTRPCRouter({
   // createComment
   // deleteComment
+
+  createComment: protectedProcedure.input(z.object({ postId: z.string(), comment: z.string(), authorId: z.string() })).mutation(async ({ctx, input}) => {
+    const { postId, comment, authorId } = input
+    const result = await ctx.prisma.comment.create({
+      data: {
+        authorId,
+        postId,
+        comment
+      }
+    })
+
+    return {
+      message: "Comment Created",
+      result
+    }
+  }),
 
   getAllComment: protectedProcedure.query(({ ctx }) => {
     return ctx.prisma.comment.findMany();
