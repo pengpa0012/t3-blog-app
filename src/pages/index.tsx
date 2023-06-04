a9852833bb882b2801d8e6f1ddae0963f6b27a16
Pengpa0012 <peng.ilikha@gmail.com>
2023-06-04 18:57:02 +0800

    update post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 dayjs.extend(relativeTime);
 
 const Home: NextPage = () => {
@@ -33,28 +34,35 @@ const Home: NextPage = () => {
         <ul className="flex mt-12 gap-2">
           {/* <li className={`text-[#bdc1c6] cursor-pointer p-3 rounded-md ${tab == 0 ? "text-white bg-white/10" : ""}`} onClick={() => setTab(0)}>For You</li>
           <li className={`text-[#bdc1c6] cursor-pointer p-3 rounded-md ${tab == 1 ? "text-white bg-white/10" : ""}`} onClick={() => setTab(1)}>Following</li> */}
-          <li className={`text-[#bdc1c6] text-2xl cursor-pointer rounded-md`}>My Feed</li>
+          <li className={`text-[#bdc1c6] text-2xl rounded-md`}>My Feed</li>
         </ul>
-        {
-          isLoading ?
-          <div role="status">
-            <svg aria-hidden="true" className="w-8 h-8 mx-auto text-gray-200 my-20 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-          :
-          allPost?.posts.map(el => (
-            <div className={`p-4 bg-[#303134] rounded-md my-4 text-[#bdc1c6]`} key={el.id} onClick={() => router.push(`/post/${el.id || ""}`)}>
-              <div className="flex justify-between items-center mb-4">
-                <h1>{el.title}</h1>
-                <h1>{dayjs(el.createdAt).fromNow()}</h1>
-              </div>
-              <p>{el.description}</p>
+        <div className="grid grid-cols-3 gap-4 my-4">
+          {
+            isLoading ?
+            <div role="status">
+              <svg aria-hidden="true" className="w-8 h-8 mx-auto text-gray-200 my-20 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                  <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+              <span className="sr-only">Loading...</span>
             </div>
-          ))
-        }
+            :
+            allPost?.posts.map(el => (
+              <div className={`p-4 bg-[#303134] rounded-md text-[#bdc1c6] cursor-pointer`} key={el.id} onClick={() => router.push(`/post/${el.id || ""}`)}>
+                <div>
+                  <div className="relative w-full h-[200px]">
+                    <Image src={el.image ?? ""} fill alt="image" className="rounded-md" />
+                  </div>
+                  <div className="flex justify-between items-center my-4">
+                    <h3 className="text-2xl">{el.title}</h3>
+                    <p>{dayjs(el.createdAt).fromNow()}</p>
+                  </div>
+                  <p>{allPost.users.find(user => user.id == el.authorId)?.name}</p>
+                </div>              
+              </div>
+            ))
+          }
+        </div>
       </main>
     </>
   );
