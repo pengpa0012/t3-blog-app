daabf35887c59f132310bbb8db48e0f24014833f
Pengpa0012 <peng.ilikha@gmail.com>
2023-06-07 08:21:34 +0800

    update env

--- src/env.mjs
+++ src/env.mjs
@@ -44,7 +44,6 @@ export const env = createEnv({
     NEXT_PUBLIC_STORAGE_BUCKET: z.string(),
     NEXT_PUBLIC_MESSAGING_SENDER_ID: z.string(),
     NEXT_PUBLIC_APP_ID: z.string(),
-    NEXT_PUBLIC_MEASUREMENT_ID: z.string(),
   },
 
   /**
@@ -69,8 +68,7 @@ export const env = createEnv({
     NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
     NEXT_PUBLIC_STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     NEXT_PUBLIC_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
-    NEXT_PUBLIC_MEASUREMENT_ID: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
